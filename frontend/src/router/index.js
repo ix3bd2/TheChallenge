b4e15b8335449fc631a3b21c8c79d4eb729948f8
AbdulH <haritani2002@gmail.com>
2022-11-09 13:47:37 +0100

    added homepage and routers v2

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -9,12 +9,13 @@ const router = createRouter({
             component: HomeView */
         },
         {
-            /*       path: '/about',
-                  name: 'about',
-                  // route level code-splitting
-                  // this generates a separate chunk (About.[hash].js) for this route
-                  // which is lazy-loaded when the route is visited.
-                  component: () => import('../views/AboutView.vue') */
+            path: '/',
+            name: 'Home',
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () =>
+                import ('../components/HomePage.vue')
         }
     ]
 })
