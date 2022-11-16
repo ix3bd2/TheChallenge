15a88ef119376de52b25c8d8d9ecf7a5323f865e
david1law <davidlaw._@outlook.com>
2022-11-16 11:44:54 +0100

    navbar fixed pos

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -20,13 +20,22 @@ export default {
 </template>
 
 <style scoped>
+    .navbar {
+        position: fixed;
+        top: 0;
+        padding: 40px;
+    }
+
     ul {
         list-style-type: none;
         margin: 0;
         padding: 0;
+        color: white;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
     }
 
     li {
         display: inline;
+        margin: 10px;
     }
 </style>
