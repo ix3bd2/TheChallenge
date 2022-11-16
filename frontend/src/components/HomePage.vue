b3bf42335ab01df60586e6ad86692d39471f0b89
AbdulH <haritani2002@gmail.com>
2022-11-16 13:29:22 +0100

    small chnages

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,27 +1,50 @@
 <template>
-<div class="home-page-main">
-  <img class="sport-equipment" src="../assets/machine.png">
-</div>
+  <div class="home-page-main">
+    <img class="sport-equipment" src="../assets/machine.png" />
+    <img class="concept-equipment" src="../assets/concept-machine.png" />
+    <div class="machine-text">
+      <p>
+        Students project to help solve the problem of affordble and clean
+        energy.
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
 <style>
-.home-page-main{
+.home-page-main {
   width: 100%;
   height: 100vh;
   background-image: url(../assets/background.jpeg);
   background-repeat: no-repeat;
   background-size: cover;
 }
-.sport-equipment{
+.sport-equipment {
+  height: 80%;
+  position: absolute;
+  left: 48%;
+  top: 50%;
+  z-index: 4;
+  transform: translate(-50%, -50%);
+}
+.machine-text {
+  color: white;
+  font-size: 17px;
   position: absolute;
   left: 50%;
-  top:50%;
-  transform: translate(-50%,-50%);
+  bottom: 5%;
+  transform: translate(-50%, -50%);
+}
+.concept-equipment {
+  height: 23%;
+  position: absolute;
+  left:54.5%;
+  top: 21%;
+  z-index: 3;
+  transform: translate(-50%, -50%);
 }
 </style>
