84b738f0f14bf8897b22fdb4c1d9ced467b5f8d0
david1law <davidlaw._@outlook.com>
2022-11-16 11:06:38 +0100

    padding fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
+import Navbar from './components/Navbar.vue'
 import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-
+  <Navbar />
 
   <RouterView />
 </template>
 
 <style scoped>
-
+  * {
+    padding: 0;
+    margin: 0;
+  }
 </style>
---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="navbar">
+    <ul>
+        <li><a href="#home">Home</a></li>
+        <li><a href="#news">News</a></li>
+        <li><a href="#contact">Contact</a></li>
+        <li><a href="#about">About</a></li>
+    </ul>
+  </div>
+</template>
