7ac4d9a5c4a62cb895f13068db42929cf5bdc4da
AbdulH <haritani2002@gmail.com>
2022-11-16 11:17:59 +0100

    bug fix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,25 @@
 <script setup>
-import Navbar from './components/Navbar.vue'
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <Navbar />
-
-  <RouterView />
+  <div>
+    <nav-bar/>
+    <RouterView />
+  </div> 
 </template>
 
+<script>
+import NavBar from "./components/Navbar.vue";
+export default {
+  components: { NavBar },
+  name: "App",
+};
+</script>
+
 <style scoped>
-  * {
-    padding: 0;
-    margin: 0;
-  }
+* {
+  padding: 0;
+  margin: 0;
+}
 </style>
--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -6,7 +6,6 @@
 
 <script>
 export default {
-  components: { Navbar },
 
 }
 </script>
