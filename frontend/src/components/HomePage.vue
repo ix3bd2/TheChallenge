98acd80fadc1abad014fbda7c45853b6f68259d7
AbdulH <haritani2002@gmail.com>
2022-11-16 11:44:51 +0100

    added equipment

--- frontend/src/components/HomePage.vue
+++ frontend/src/components/HomePage.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="home-page-main">
-
+  <img class="sport-equipment" src="../assets/machine.png">
 </div>
 </template>
 
@@ -15,6 +15,13 @@ export default {
   width: 100%;
   height: 100vh;
   background-image: url(../assets/background.jpeg);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
-
+.sport-equipment{
+  position: absolute;
+  left: 50%;
+  top:50%;
+  transform: translate(-50%,-50%);
+}
 </style>
