c2bc918f54116e5f3828c1d241feb41a32c079fd
david1law <davidlaw._@outlook.com>
2022-11-23 11:56:28 +0100

    fix logo navbar

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -12,7 +12,7 @@ export default {
   <div class="container">
     <header id="NavBar" class="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
       <a href="/" class="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
-        <img src="../assets/EfreeLogo.png" alt="" height="100">
+        <img src="../assets/EfreeLogo.png" alt="" height="50">
       </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
