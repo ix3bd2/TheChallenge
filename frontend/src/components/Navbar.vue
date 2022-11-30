b79a63eaa6d46d6ec0361d00999b835bf72083e4
david1law <davidlaw._@outlook.com>
2022-11-30 14:18:08 +0100

    corrected nav link

--- frontend/src/components/Navbar.vue
+++ frontend/src/components/Navbar.vue
@@ -24,49 +24,21 @@ export default {
 
 <template>
   <div class="container">
-    <header
-       v-if="!show"
-      id="NavBar"
-      class="
-        d-flex
-        flex-wrap
-        align-items-center
-        justify-content-center justify-content-md-between
-        py-3
-        mb-4
-      "
-    >
-      <a
-        href="/"
-        class="
-          d-flex
-          align-items-center
-          col-md-3
-          mb-2 mb-md-0
-          text-dark text-decoration-none
-        "
-      >
+    <header v-if="!show" id="NavBar" class=" d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4">
+      <a href="/" class=" d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
         <img src="../assets/EfreeLogo.png" alt="" height="50" />
       </a>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
-        <li><a href="#" class="nav-link px-4 link-light fw-bold">Home</a></li>
-        <li>
-          <a href="#" class="nav-link px-4 link-light fw-bold">About us</a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-4 link-light fw-bold">Order now</a>
-        </li>
-        <li>
-          <a href="#" class="nav-link px-4 link-light fw-bold">Partners</a>
-        </li>
+        <li><a href="/" class="nav-link px-4 link-light fw-bold">Home</a></li>
+        <li><a href="#" class="nav-link px-4 link-light fw-bold">About us</a></li>
+        <li><a href="#" class="nav-link px-4 link-light fw-bold">Order now</a></li>
+        <li><a href="#" class="nav-link px-4 link-light fw-bold">Partners</a></li>
       </ul>
 
       <div class="col-md-3 text-end">
         <a href="/login">
-        <button type="button" class="btn btn-outline-primary me-2 fw-bold">
-          Login
-        </button>
+        <button type="button" class="btn btn-outline-primary me-2 fw-bold">Login</button>
         </a>
       </div>
     </header>
