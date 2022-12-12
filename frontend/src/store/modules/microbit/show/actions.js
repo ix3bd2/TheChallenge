669918b6b6532d45642a0705e65a7e4fe700a72c
AbdulH <haritani2002@gmail.com>
2022-12-12 17:52:01 +0100

    small change

--- frontend/src/store/modules/microbit/show/actions.js
+++ frontend/src/store/modules/microbit/show/actions.js
@@ -3,28 +3,34 @@ import { extractHubURL } from "../../../../utils/mercure";
 import * as types from "./mutation_types";
 
 export const retrieve = ({ commit }, id) => {
-  commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
+    commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
 
-  return fetch(id)
-    .then((response) =>
-      response.json().then((data) => ({
-        data,
-        hubUrl: extractHubURL(response),
-      }))
-    )
-    .then(({ data, hubUrl }) => {
-      commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
-      commit(types.MICROBIT_SHOW_SET_RETRIEVED, data);
-      if (hubUrl) {
-        commit(types.MICROBIT_SHOW_SET_HUB_URL, hubUrl);
-      }
-    })
-    .catch((e) => {
-      commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
-      commit(types.MICROBIT_SHOW_SET_ERROR, e.message);
-    });
+    return fetch(id, {
+            method: "GET",
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${sessionStorage.getItem("token")}`,
+            }),
+        })
+        .then((response) =>
+            response.json().then((data) => ({
+                data,
+                hubUrl: extractHubURL(response),
+            }))
+        )
+        .then(({ data, hubUrl }) => {
+            commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
+            commit(types.MICROBIT_SHOW_SET_RETRIEVED, data);
+            if (hubUrl) {
+                commit(types.MICROBIT_SHOW_SET_HUB_URL, hubUrl);
+            }
+        })
+        .catch((e) => {
+            commit(types.MICROBIT_SHOW_TOGGLE_LOADING);
+            commit(types.MICROBIT_SHOW_SET_ERROR, e.message);
+        });
 };
 
 export const reset = ({ commit }) => {
-  commit(types.MICROBIT_SHOW_RESET);
+    commit(types.MICROBIT_SHOW_RESET);
 };
