863df84c9f58c7332e10d48b5a055b66b216db08
AbdulH <haritani2002@gmail.com>
2022-12-25 19:53:03 +0100

    added HEC

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -2,39 +2,60 @@
   <div class="dashboard">
     <h1>Dashboard</h1>
     <div class="row">
-      <div class="col col-lg-4 admin-dashboard-card">
+      <div class="col col-lg-3 admin-dashboard-card">
         <div class="admin-card">
-          <h5 class="card-header-text">Total income</h5>
-          <h5 class="card-header-text">{{this.totalKosten}}</h5>
+          <h6 class="card-header-text">Total income</h6>
+          <h6 class="card-header-text">{{ this.totalKosten }}</h6>
         </div>
       </div>
-      <div class="col col-lg-4 admin-dashboard-card">
+      <div class="col col-lg-3 admin-dashboard-card">
         <div class="admin-card">
-          <h5 class="card-header-text">Total sold equipment</h5>
+          <h6 class="card-header-text">Total sold equipment</h6>
+          <h6 class="card-header-text">{{ this.totalSold }}</h6>
         </div>
       </div>
-      <div class="col col-lg-4 admin-dashboard-card">
+      <div class="col col-lg-3 admin-dashboard-card">
         <div class="admin-card">
-          <h5 class="card-header-text">Total client</h5>
+          <h6 class="card-header-text">Total client</h6>
+          <h6 class="card-header-text">{{ this.totalClients }}</h6>
+        </div>
+      </div>
+      <div class="col col-lg-3 admin-dashboard-card" style="position: relative">
+        <div class="admin-card">
+          <h6 class="card-header-text">heighest energy genrating clients</h6>
+
+          <Doughnut
+            v-if="chartData.datasets[0].data"
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+          />
+          <div v-else>Loading...</div>
         </div>
       </div>
     </div>
     <div class="row">
-      <div class="col col-lg-6 admin-dashboard-card">
+      <div class="col col-lg-6 admin-dashboard-card" style="position: relative">
         <div class="admin-card">
-          <h5 class="card-header-text">heighest energy genrating clients</h5>
+          <h6 class="card-header-text">heighest energy genrating clients</h6>
+
+          <Doughnut
+            id="my-chart-id"
+            :options="chartOptions"
+            :data="chartData"
+          />
         </div>
       </div>
       <div class="col col-lg-6 admin-dashboard-card">
         <div class="admin-card">
-          <h5 class="card-header-text">Total Generated Energy</h5>
+          <h6 class="card-header-text">Total Generated Energy</h6>
         </div>
       </div>
     </div>
     <div class="row">
       <div class="col col-lg-12 admin-dashboard-card">
         <div class="admin-card" style="height: 350px">
-          <h5 class="card-header-text">latest clients</h5>
+          <h6 class="card-header-text">latest clients</h6>
         </div>
       </div>
     </div>
@@ -44,12 +65,49 @@
 <script>
 import axios from "axios";
 import { ENTRYPOINT } from "../config/entrypoint";
+import { Doughnut } from "vue-chartjs";
+
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  ArcElement,
+  CategoryScale,
+} from "chart.js";
+ChartJS.register(Title, Tooltip, Legend, ArcElement, CategoryScale);
+
 export default {
-  name: "Login",
+  components: { Doughnut },
+
   data() {
     return {
       totalIncome: null,
       totalKosten: null,
+      totalClients: null,
+      douData: false,
+      totalSold: null,
+      HEC: [],
+      HECN: [],
+      HECC: [],
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            backgroundColor: ["#e78200", "#41B883", "#00D8FF", "#DD1B16"],
+            data: null,
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: "bottom",
+          },
+        },
+      },
     };
   },
   created() {
@@ -59,10 +117,31 @@ export default {
       )
       .then((response) => {
         var array1 = response.data["hydra:member"].length;
+        this.totalClients = response.data["hydra:member"].length;
         this.totalIncome = response.data["hydra:member"];
         for (var i = 0; i < array1; i++) {
-         this.totalKosten += this.totalIncome[i].kosten;
+          this.HEC.push(this.totalIncome[i].TotalGen);
+          this.HECC.push(
+            "#" + Math.floor(Math.random() * 16777215).toString(16)
+          );
+
+          if (this.HECN.length < 3)
+            this.HECN.push(this.totalIncome[i].user.username);
+          this.totalKosten += this.totalIncome[i].kosten;
         }
+        this.chartData.datasets[0].data = this.HEC;
+        this.chartData.labels = this.HECN;
+        this.chartData.datasets[0].backgroundColor = this.HECC;
+      });
+
+    axios
+      .get(ENTRYPOINT + "micro_bits", {
+        headers: {
+          Authorization: `Bearer ${sessionStorage.getItem("token")}`,
+        },
+      })
+      .then((response) => {
+        this.totalSold = response.data["hydra:member"].length;
       });
   },
 };
@@ -95,5 +174,11 @@ export default {
   font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
 
   text-align: start;
+}
+#my-chart-id {
+  position: absolute;
+  top: 20%;
+  height: 65% !important;
+  width: 90% !important;
 }
 </style>
